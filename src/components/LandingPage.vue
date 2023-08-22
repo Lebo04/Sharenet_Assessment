b83b2b513e7deab7b82c7e7c0eb541e1850b0e1b
Lebo04 <nobomvukelebogile497@gmail.com>
2023-08-22 16:21:39 +0200

    first commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <h1>Crypto Prices</h1>
+    <table>
+      <thead>
+        <tr>
+          <th @click="sort('name')">Coin Name</th>
+          <th @click="sort('symbol')">Coin Symbol</th>
+          <th @click="sort('current_price')">Price</th>
+          <th @click="sort('price_change_percentage_1h_in_currency')">1h Move</th>
+          <th @click="sort('market_cap')">Market Cap</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="crypto in sortedCryptoData" :key="crypto.id">
+          <td>{{ crypto.name }}</td>
+          <td>{{ crypto.symbol }}</td>
+          <td>{{ crypto.current_price }}</td>
+          <td>{{ crypto.price_change_percentage_1h_in_currency }}%</td>
+          <td>{{ crypto.market_cap }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <router-link to="/page2">Go to Page 2</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      cryptoData: [],
+      sortBy: null,
+      sortDirection: "asc",
+    };
+  },
+  computed: {
+    sortedCryptoData() {
+      const data = [...this.cryptoData];
+      if (this.sortBy) {
+        data.sort((a, b) => {
+          const modifier = this.sortDirection === "asc" ? 1 : -1;
+          return (
+            modifier *
+            (a[this.sortBy] - b[this.sortBy])
+          );
+        });
+      }
+      return data;
+    },
+  },
+  methods: {
+    async fetchData() {
+      const response = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets",
+        {
+          params: {
+            vs_currency: "usd",
+            order: "market_cap_desc",
+            per_page: 10,
+            page: 1,
+          },
+        }
+      );
+      this.cryptoData = response.data;
+    },
+    sort(column) {
+      if (this.sortBy === column) {
+        this.sortDirection = this.sortDirection === "asc" ? "desc" : "asc";
+      } else {
+        this.sortBy = column;
+        this.sortDirection = "asc";
+      }
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
+
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+th,
+td {
+  border: 1px solid #ccc;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  cursor: pointer;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+router-link {
+  display: block;
+  margin-top: 20px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LandingPage from "../components/LandingPage.vue";
+import ContactUs from "../components/ContactUs";
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: LandingPage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/contact',
+    name: 'contact',
+    component: ContactUs
   }
 ]
 
